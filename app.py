9269ca186f84844f4a5b18fd9af4dbe6d65346a8
htrangn <124483392+htrangn@users.noreply.github.com>
2023-03-11 10:37:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ a = st.number_input('Tham số a')
 b = st.number_input('Tham số b')
 
 if st.button('Giải'):
-  if (a!=0) st.write('Phương trình có 1 nghiệm', -b/a)
-  if (a==0 and b==0) st.write('Phương trình có vô số nghiệm')
-  if (a==0 and b!=0) st.write('Phương trình vô nghiệm')
+  if (a!=0): st.write('Phương trình có 1 nghiệm', -b/a)
+  if (a==0 and b==0): st.write('Phương trình có vô số nghiệm')
+  if (a==0 and b!=0): st.write('Phương trình vô nghiệm')
